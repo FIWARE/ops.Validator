7001b1e6cb9cec3cf208e8995e1adf1dedb98f4f
pmverdugo <pmverdugo@dit.upm.es>
2016-03-15 20:21:52 +0100

    webui skeleton

---
+++ bork/command/validator_api.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+"""
+Chef Validator API Server. An OpenStack ReST API to Validate Chef Cookbooks.
+"""
+
+import os
+import sys
+import six
+import oslo_i18n as i18n
+from oslo_config import cfg
+
+# default path
+if 'config_dir' not in cfg.CONF:
+    cfg.CONF.config_dir = "/etc/bork"
+
+from bork.common import log as logging
+from bork.common.i18n import _LI
+from bork.common import config
+from bork.common import wsgi
+
+i18n.enable_lazy()
+
+LOG = logging.getLogger()
+CONF = config.CONF
+
+
+def main():
+    """Launch validator API """
+    try:
+        config.parse_args()
+        logging.setup(CONF, 'validator_api')
+        app = config.load_paste_app("validator_api")
+        port, host = (CONF.bind_port, CONF.bind_host)
+        LOG.info(_LI('Starting Validator ReST API on %(host)s:%(port)s'),
+                 {'host': host, 'port': port})
+        server = wsgi.Service(app, port, host)
+        server.start()
+        server.wait()
+    except RuntimeError as e:
+        msg = six.text_type(e)
+        sys.exit("ERROR: %s" % msg)
+
+
+if __name__ == '__main__':
+    main()
---
+++ bork/tests/unit/api/middleware/test_ssl.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+"""Tests for validator.api.middleware.ssl """
+
+import mock
+import webob
+from bork.api.middleware.ssl import SSLMiddleware
+from bork.tests.unit.base import ValidatorTestCase
+
+
+class SSLMiddlewareTestCase(ValidatorTestCase):
+    """Tests for class SSLMiddleware """
+
+    def setUp(self):
+        """Create a SSLMiddleware instance """
+        super(SSLMiddlewareTestCase, self).setUp()
+        self.item = SSLMiddleware(None)
+
+    def test_process_request(self):
+        """Tests for method process_request """
+        self.item.external = mock.MagicMock()
+        input = webob.Request.blank('/test', headers={})
+        expected = None
+        self.item.external.return_value = None
+        observed = self.item.process_request(input)
+        self.assertEqual(expected, observed)
+
+    def tearDown(self):
+        """Cleanup the SSLMiddleware instance """
+        super(SSLMiddlewareTestCase, self).tearDown()
+        self.m.UnsetStubs()
+        self.m.ResetAll()
