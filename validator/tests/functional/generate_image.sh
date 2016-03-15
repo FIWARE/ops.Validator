b8d08e83bbc8bef29a0e8890b66e3f9c9f80b50d
pmverdugo <pmverdugo@dit.upm.es>
2016-03-15 19:50:15 +0100

    setup update

--- validator/tests/functional/generate_image.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python ../../command/generate_image_test.py --url=tcp://127.0.0.1:2375 --image=ChefImage.docker
--- validator/tests/functional/test_deploy/docker-keystone/keystone.sql
+++
@@ -1,4 +0,0 @@
-drop database  if exists keystone;
-create database keystone;
-GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'KEYSTONE_DBPASS';
-GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'KEYSTONE_DBPASS';
