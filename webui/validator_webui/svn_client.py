7b8173274a0c38b1c064a6bd8a2c387309129dbe
pmverdugo <pmverdugo@dit.upm.es>
2016-03-28 11:37:19 +0200

    subversion cookbook detection

--- webui/validator_webui/svn_client.py
+++ webui/validator_webui/svn_client.py
@@ -2,6 +2,8 @@
 """
 Cookbook Repository Management
 """
+import logging
+
 import svn.remote
 
 
@@ -17,24 +19,41 @@ class CookbookRepo:
         :param pwd: password
         :param url: url
         """
-        svn_repo = 'https://{user}:{pwd}@{url}'.format(
-            user=user,
-            pwd=pwd,
-            url=url
-        )
-        print svn_repo
+        if url.startswith("https://"):
+            svn_repo = url
+        else:
+            svn_repo = 'https://{user}:{pwd}@{url}'.format(
+                user=user,
+                pwd=pwd,
+                url=url
+            )
         self.r = svn.remote.RemoteClient(svn_repo)
 
-    def list_cookbooks(self):
+    def list_cookbooks(self, rp=None):
         """
         :return: List of all cookbooks in repo
         """
         cookbooks = []
-        entries = self.r.list()
+        entries = self.r.list(rel_path=rp)
         for filename in entries:
-            print filename
-            cookbooks.append(filename)
+            if self.check_cookbook(filename):
+                cookbooks.append(filename.replace("/", ""))
         return cookbooks
+
+    def check_cookbook(self, name):
+        """check if the item is a cookbook"""
+        logging.info("checking %s" % name)
+        check = False
+        # check if the item is a directory
+        res = self.info(rel_path=name)
+        if res['entry_kind'] == 'dir':
+            # check if the item has a recipes directory
+            if "recipes/" in self.r.list(rel_path=name):
+                check = True
+                logging.debug("Cookbook found: %s" % name)
+        if not check:
+            logging.debug("Not a cookbook: %s" % name)
+        return check
 
     def download_cookbooks(self, local_path='/tmp/cookbooks'):
         """
@@ -44,11 +63,14 @@ class CookbookRepo:
         """
         return self.r.export(local_path)
 
-    def info(self):
-        return self.r.info()
+    def info(self, rel_path=None):
+        return self.r.info(rel_path=rel_path)
 
 if __name__ == '__main__':
     import sys
+
+    logging.basicConfig(level=logging.DEBUG)
     c = CookbookRepo(*sys.argv[1:])
-    print c.info()
-    c.list_cookbooks()
+    print c.list_cookbooks()
+    c.download_cookbooks()
+
