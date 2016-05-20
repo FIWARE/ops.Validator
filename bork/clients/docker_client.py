b4ef884fcb15abf4920e7090f10b68e1a0f25c9d
pmverdugo <pmverdugo@dit.upm.es>
2016-05-20 13:37:17 +0200

    functional test containers update

--- bork/clients/docker_client.py
+++ bork/clients/docker_client.py
@@ -2,8 +2,12 @@
 """
 Dockerfile Management
 """
+import os
+import re
 import logging
 from oslo_config import cfg
+
+LOG = logging.getLogger(__name__)
 
 
 class DockerClient:
@@ -15,4 +19,17 @@ class DockerClient:
         self.dockerfile_path = cfg.CONF.config_dir
 
     def list_systems(self):
-        return self.dockerfile_path
+        """
+        List current supported systems
+        :return:
+        """
+        systems = set()
+        LOG.debug("Searching supported systems in %s" % self.dockerfile_path)
+        for df in os.listdir(self.dockerfile_path):
+            if os.path.splitext(df)[1] == ".dockerfile":
+                with open(os.path.abspath(df)) as dff:
+                    cont = dff.read()
+                    m = re.match("FROM ([^:]+):([^:]+)\n", cont)
+                    if m:
+                        systems.add({m.group(1): m.group(2)})
+        return systems
