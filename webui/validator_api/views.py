64c2af760b1c8b8ebd48bf62a28a3b4530855ecc
pmverdugo <pmverdugo@dit.upm.es>
2016-05-23 19:05:40 +0200

    Django RESTfull framwork API implementation

---
+++ webui/validator_api/views.py
@@ -0,0 +1,29 @@
+from rest_framework import viewsets
+from validator_api.models import System, Repo, CookBook, Recipe, Deployment
+from validator_api.serializers import SystemSerializer, RepoSerializer, CookBookSerializer, RecipeSerializer, DeploymentSerializer
+
+
+class SystemViewSet(viewsets.ModelViewSet):
+    queryset = System.objects.all()
+    serializer_class = SystemSerializer
+
+
+class RepoViewSet(viewsets.ModelViewSet):
+    queryset = Repo.objects.all()
+    serializer_class = RepoSerializer
+
+
+class CookBookViewSet(viewsets.ModelViewSet):
+    queryset = CookBook.objects.all()
+    serializer_class = CookBookSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class DeploymentViewSet(viewsets.ModelViewSet):
+    queryset = Deployment.objects.all()
+    serializer_class = DeploymentSerializer
+
--- webui/webui/urls.py
+++ webui/webui/urls.py
@@ -1,23 +1,16 @@
-"""webui URL Configuration
+# coding=utf-8
+from validator_api import views
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
-from django.contrib import admin
-
-from validator_webui import views
+router = DefaultRouter()
+router.register(r'systems', views.SystemViewSet)
+router.register(r'repos', views.RepoViewSet)
+router.register(r'cookbooks', views.CookBookViewSet)
+router.register(r'recipes', views.RecipeViewSet)
+router.register(r'deployments', views.DeploymentViewSet)
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^', include(router.urls)),
 ]
