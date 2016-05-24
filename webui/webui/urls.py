81d8c4553486e8e28318c50f6a34153785d1f528
pmverdugo <pmverdugo@dit.upm.es>
2016-05-24 11:55:21 +0200

    RESTfull framework Images API extension

--- webui/webui/urls.py
+++ webui/webui/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'systems', views.SystemViewSet)
+router.register(r'images', views.ImageViewSet)
 router.register(r'repos', views.RepoViewSet)
 router.register(r'cookbooks', views.CookBookViewSet)
 router.register(r'recipes', views.RecipeViewSet)
