a2c7ca87c6b92ed4268b6b2446c8af5f332e0c9c
pmverdugo <pmverdugo@dit.upm.es>
2016-05-25 19:19:46 +0200

    django server refactor

---
+++ validator_webui/webui/urls.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from validator_api import views
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from django.contrib import admin
+admin.autodiscover()
+
+router = DefaultRouter()
+router.register(r'systems', views.SystemViewSet)
+router.register(r'images', views.ImageViewSet)
+router.register(r'repos', views.RepoViewSet)
+router.register(r'cookbooks', views.CookBookViewSet)
+router.register(r'recipes', views.RecipeViewSet)
+router.register(r'deployments', views.DeploymentViewSet)
+
+urlpatterns = [
+    # url(r'^$', views.index, name='index'),
+    # url(r'^admin/', include(admin.site.urls)),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # url(r'^', include('openstack_auth.urls')),
+    url(r'^', include(router.urls)),
+]
