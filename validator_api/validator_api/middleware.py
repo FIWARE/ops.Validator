2045eb0064b345631b0ee8e8a86cfa6d8311519e
pmverdugo <pmverdugo@dit.upm.es>
2016-05-25 19:19:46 +0200

    django server refactor

--- validator_api/validator_api/middleware.py
+++ validator_api/validator_api/middleware.py
@@ -5,7 +5,7 @@ from openstack_auth.exceptions import KeystoneAuthException
 
 class KeystoneAuthExceptionMiddleware(KeystoneAuthException):
     def process_exception(self, request, exception):
-        if (isinstance(exception, KeystoneAuthException) and exception.message == u"Invalid credentials."):
+        if isinstance(exception, KeystoneAuthException) and exception.message == u"Invalid credentials.":
             return HttpResponse('Unauthorized', status=401)
 
 
